27d026696244c06cbf16fc843a5652910984661c
pedro1miranda <pedromjorge2005@gmail.com>
2025-05-16 12:36:01 -0300

    Inicio do projeto

---
+++ appHome/urls.py
@@ -0,0 +1,12 @@
+#importa a função .path no modulo django.urls
+from django.urls import path;
+
+#Importa o modulo views no diretorio atual;
+from . import views;
+
+#lista de objetos path, define padrões urls
+
+urlpatterns = [
+    path("appHome/", views.appHome, name="appHome"),
+]
+
---
+++ appHome/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# importe a classe HttpResponse do modulo Http dentro do pacote Django
+
+from django.http import HttpResponse
+
+#Função que cria a view
+
+def appHome(request):
+    return HttpResponse("ola mundo")
